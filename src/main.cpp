69b38c6cdd8c0cdd9420f84988fccb472ee0c35c
Alfred Längheim <alfred.langheim@gmail.com>
2020-09-04 15:12:14 +0200

    Voltlampor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,42 @@
 #include <Arduino.h>
 
+#define LEDGREEN 3
+#define LEDRED 2
+#define ANALOGBATTERY A0  
+
+int analogValue = 0;
+float voltage = 0;
+int ledDelay = 800;
+
+float maximum  = 1.5;
+float minimum  = 1.0;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode (LEDGREEN, OUTPUT);
+  pinMode (LEDRED, OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  analogValue = analogRead (ANALOGBATTERY);
+
+  voltage = 0.0048 * analogValue;
+  Serial.print("Voltage:");
+  Serial.println (voltage);
+  
+  if (voltage>= maximum)
+  {
+    digitalWrite (LEDGREEN, HIGH);
+    delay(ledDelay);
+    digitalWrite (LEDGREEN, LOW);
+  }
+  else if (voltage <maximum && voltage> minimum)
+  {
+    digitalWrite(LEDRED, HIGH);
+    delay(ledDelay);
+    digitalWrite(LEDRED, LOW);
+  }
 }
